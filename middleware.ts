02b50200b7d1c19fa2f7e6ddd6b55b07e82a2698
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-02-03 14:06:15 +0100

    Gjør om middleware til dummy
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,34 @@
 // middleware.ts
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import { validerToken } from './auth/verifyAccessToken';
 
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
-  const loginPath = `/oauth2/login?redirect=${url}/`;
-
-  const { get } = request.headers;
-  const authorization = get('authorization');
-
+  // const loginPath = `/oauth2/login?redirect=${url}/`;
+  //
+  // const { get } = request.headers;
+  // const authorization = get('authorization');
   // if (url?.includes('isReady') || url.includes('isAlive')) {
   //   return NextResponse.next();
-  // }œ
-
-  if (!authorization || !(await validateAuthorization(authorization))) {
-    return NextResponse.redirect(loginPath);
-  } else {
-    return NextResponse.rewrite(url);
-  }
+  // }
+  //
+  // if (!authorization || !(await validateAuthorization(authorization))) {
+  //   return NextResponse.redirect(loginPath);
+  // } else {
+  return NextResponse.rewrite(url);
+  // }
 }
 
-const validateAuthorization = async (authorization: string) => {
-  try {
-    const token = authorization.split(' ')[1];
-    const JWTVerifyResult = await validerToken(token);
-    return !!JWTVerifyResult?.payload;
-  } catch (e) {
-    // LogError('azure ad error', e);
-    return false;
-  }
-};
+// const validateAuthorization = async (authorization: string) => {
+//   try {
+//     const token = authorization.split(' ')[1];
+//     const JWTVerifyResult = await validerToken(token);
+//     return !!JWTVerifyResult?.payload;
+//   } catch (e) {
+//     // LogError('azure ad error', e);
+//     return false;
+//   }
+// };
 
 // See "Matching Paths" below to learn more
 export const config = {
