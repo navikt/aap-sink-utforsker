3a937af90f168e27de8418554dae182cca5bde35
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-02-03 10:18:26 +0100

    Legger til endepunkt for å hente kafak topic

--- pages/sok/index.tsx
+++ pages/sok/index.tsx
@@ -1,26 +1,57 @@
-import { Search } from '@navikt/ds-react';
-import { useEffect, useState } from 'react';
+import { Button, Search, TextField } from '@navikt/ds-react';
+import { useState } from 'react';
+import { Buffer } from 'buffer';
 
 const Søk = () => {
-  const [value, setValue] = useState<string>();
-  const [data, setData] = useState();
+  const [value, setValue] = useState<string>('');
+  const [data, setData] = useState<any[]>([]);
 
-  useEffect(() => {
-    fetch('api/?antall=1&retning=DESC', {
+  const parseJSON = (data: string) => {
+    if (!data) {
+      return 'No JSON here...';
+    }
+    try {
+      const buf = Buffer.from(data, 'base64');
+      return JSON.parse(buf.toString());
+    } catch (error) {
+      console.error('Klarte ikke å parse json. ' + error);
+      console.error(data);
+    }
+  };
+
+  async function doFetch(personident: string) {
+    const params = new URLSearchParams({
+      antall: '1',
+      retning: 'DESC',
+    });
+
+    const response = await fetch('api/?' + params, {
       method: 'GET',
       headers: {
-        personident: '11457841185',
+        personident: personident,
         accept: 'application/json',
       },
-    }).then((res) => res.json().then((data) => setData(data)));
-  }, []);
+    });
 
-  console.log(data);
+    if (response.ok) {
+      const json: any[] = await response.json();
+
+      const parsedRespons = json.map((obj) => {
+        obj.record = parseJSON(obj.record);
+        return obj;
+      });
+
+      setData(parsedRespons);
+    }
+  }
 
   return (
-    <div>
-      <Search label={'Skriv inn noe her'} onChange={(e) => setValue(e)} variant={'simple'} />
-      <h2>{value}</h2>
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr', gridRowGap: '1rem' }}>
+      <div style={{ width: '50%', display: 'grid', gridTemplateColumns: '1fr', gridRowGap: '1rem' }}>
+        <TextField label={'Skriv inn noe her'} onChange={(e) => setValue(e.target.value)} />
+        <Button onClick={() => doFetch(value)}>Søk</Button>
+      </div>
+      {data && data.length > 0 && <div style={{ padding: '1rem' }}>{JSON.stringify(data)}</div>}
     </div>
   );
 };
