0ad7f87bc1128f82adda88c9defebd978712a8c5
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-02-06 13:15:30 +0100

    Legger til validering på autorisasjon token
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 // middleware.ts
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import { validerToken } from './auth/verifyAccessToken';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   console.log(request.nextUrl.pathname, request.nextUrl.basePath);
   console.log('request nexturl pathname', request.nextUrl.pathname);
@@ -16,39 +17,25 @@ export function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  if (authorization) {
+  if (authorization && (await validateAuthorization(authorization))) {
     console.log('has auth header');
+    NextResponse.next();
   } else {
     console.log('has no auth header');
     return NextResponse.redirect(loginPath);
   }
-
-  // return response;
-
-  // const url = request.nextUrl.clone();
-  // const loginPath = `/oauth2/login?redirect=${url}/`;
-  // const { get } = request.headers;
-  // const authorization = get('authorization');
-  // console.log('requestUrl', request.url);
-  // console.log('url', url);
-  // console.log('authorization', authorization);
-  // if (!authorization) {
-  //   return NextResponse.rewrite(loginPath);
-  // } else {
-  //   return NextResponse.rewrite(url);
-  // }
 }
 
-// const validateAuthorization = async (authorization: string) => {
-//   try {
-//     const token = authorization.split(' ')[1];
-//     const JWTVerifyResult = await validerToken(token);
-//     return !!JWTVerifyResult?.payload;
-//   } catch (e) {
-//     // LogError('azure ad error', e);
-//     return false;
-//   }
-// };
+const validateAuthorization = async (authorization: string) => {
+  try {
+    const token = authorization.split(' ')[1];
+    const JWTVerifyResult = await validerToken(token);
+    return !!JWTVerifyResult?.payload;
+  } catch (e) {
+    // LogError('azure ad error', e);
+    return false;
+  }
+};
 
 // See "Matching Paths" below to learn more
 export const config = {
