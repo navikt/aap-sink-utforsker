51a949b8a0949309d38463cf3ae021bb5453563d
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-02-07 13:36:39 +0100

    Fjerner rewrites fra next config
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@ const nextConfig = {
   trailingSlash: false,
   reactStrictMode: true,
   output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3001/søker/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
