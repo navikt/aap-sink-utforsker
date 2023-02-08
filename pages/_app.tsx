a8bf3591229aa13fd941fb5babe10e4fc84b1235
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-02-08 15:31:30 +0100

    Legger til swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import '@navikt/ds-css';
 import '@navikt/ds-css-internal';
 import '../src/styles/index.css';
 import '../src/styles/globals.css';
+import { SWRConfig } from 'swr/_internal';
 
 function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -16,11 +17,13 @@ function App(props: AppProps) {
         <title>AAP Sink Utforsker</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Layout>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </Layout>
+      <SWRConfig value={{ fetcher: (url: string) => fetch(url).then((res) => res.json()) }}>
+        <Layout>
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </Layout>
+      </SWRConfig>
     </>
   );
 }
