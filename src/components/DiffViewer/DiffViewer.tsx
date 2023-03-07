26b9c33d291ee5fdba05845c97951e2845a111f9
Øivind Stensrud <oivind.stensrud@nav.no>
2023-03-07 13:16:50 +0100

    Mulighet for å diffe to rader i søkeresultatet

---
+++ src/components/DiffViewer/DiffViewer.tsx
@@ -0,0 +1,93 @@
+import { Button, Modal } from '@navikt/ds-react';
+import { useState } from 'react';
+import { ResultatType } from '../../../pages/sok';
+// import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer-continued';
+import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer';
+
+import styles from './DiffViewer.module.css';
+
+interface Props {
+  leftSide?: ResultatType;
+  rightSide?: ResultatType;
+  clearAll: Function;
+}
+interface DiffVisningProps {
+  leftSide?: ResultatType;
+  rightSide?: ResultatType;
+}
+
+const DiffVisning = ({ leftSide, rightSide }: DiffVisningProps) => {
+  if (!leftSide || !rightSide) {
+    return <div>Mangler dokumenter for å kunne gjøre en diff</div>;
+  }
+  return (
+    <ReactDiffViewer
+      oldValue={JSON.stringify(leftSide.record, null, 2)}
+      newValue={JSON.stringify(rightSide.record, null, 2)}
+      compareMethod={DiffMethod.WORDS}
+      leftTitle={
+        <div>
+          <div>Partition: {leftSide.partition}</div>
+          <div>Offset: {leftSide.offset}</div>
+        </div>
+      }
+      rightTitle={
+        <div>
+          <div>Partition: {rightSide.partition}</div>
+          <div>Offset: {rightSide.offset}</div>
+        </div>
+      }
+    />
+  );
+};
+
+const SummaryRow = ({ noekkel, verdi }: { noekkel: string; verdi?: number }) => (
+  <div className={styles.summaryRow}>
+    <span className={styles.key}>{noekkel}</span>
+    <span className={styles.value}>{verdi}</span>
+  </div>
+);
+const DiffSummary = ({ leftSide, rightSide }: { leftSide?: ResultatType; rightSide?: ResultatType }) => {
+  const sider = [
+    { side: 'Venstre', object: leftSide },
+    { side: 'Høyre', object: rightSide },
+  ];
+  return (
+    <div className={styles.summary}>
+      {sider.map((side, index) => (
+        <div key={index} className={styles.diffSide}>
+          <div className={styles.bold}>{side.side}</div>
+          <SummaryRow noekkel={'Partition'} verdi={side.object?.partition} />
+          <SummaryRow noekkel={'Offset'} verdi={side.object?.offset} />
+        </div>
+      ))}
+    </div>
+  );
+};
+const DiffViewer = ({ leftSide, rightSide, clearAll }: Props) => {
+  const [diffOpen, toggleDiffOpen] = useState<boolean>(false);
+  if (!leftSide && !rightSide) {
+    return null;
+  }
+
+  return (
+    <section className={styles.diffContainer}>
+      <div className={styles.stickyContainer}>
+        <DiffSummary leftSide={leftSide} rightSide={rightSide} />
+        <div className={styles.buttonRow}>
+          <Button variant={'primary'} onClick={() => (leftSide && rightSide ? toggleDiffOpen(true) : null)}>
+            Vis diff
+          </Button>
+          <Button onClick={() => clearAll()} variant={'secondary'}>
+            Lukk
+          </Button>
+        </div>
+      </div>
+      <Modal open={diffOpen} onClose={() => toggleDiffOpen(false)} className={styles.modal}>
+        <DiffVisning leftSide={leftSide} rightSide={rightSide} />
+      </Modal>
+    </section>
+  );
+};
+
+export { DiffViewer };
--- src/components/Soekeresultat/Soekeresultat.tsx
+++ src/components/Soekeresultat/Soekeresultat.tsx
@@ -1,4 +1,4 @@
-import { Table } from '@navikt/ds-react';
+import { Button, Table } from '@navikt/ds-react';
 
 import styles from './Soekeresultat.module.css';
 import { ResultatType } from '../../../pages/sok';
@@ -7,6 +7,16 @@ import 'react-json-view-lite/dist/index.css';
 import { sortData, useHandleSort } from '@/components/Soekeresultat/SoekeresultatUtil';
 import { CopyToClipboard } from '@navikt/ds-react-internal';
 import { StatusIcon } from './StatusIcon';
+import { DiffViewer } from '@/components/DiffViewer';
+import { useReducer } from 'react';
+
+enum DiffActions {
+  ADD_RIGHT = 'ADD_RIGHT',
+  ADD_LEFT = 'ADD_LEFT',
+  REMOVE_RIGHT = 'REMOVE_RIGHT',
+  REMOVE_LEFT = 'REMOVE_LEFT',
+  CLEAR_ALL = 'CLEAR_ALL',
+}
 
 const ExpandableContent = ({ data }: { data: string | object }) => {
   if (typeof data === 'string') {
@@ -17,9 +27,11 @@ const ExpandableContent = ({ data }: { data: string | object }) => {
 
 interface DataradProps {
   rad: ResultatType;
+  addLeft: Function;
+  addRight: Function;
 }
 const Datarad = (props: DataradProps) => {
-  const { rad } = props;
+  const { rad, addLeft, addRight } = props;
   return (
     <Table.ExpandableRow content={<ExpandableContent data={rad.record} />}>
       <Table.DataCell>
@@ -36,6 +48,14 @@ const Datarad = (props: DataradProps) => {
       <Table.DataCell>
         <CopyToClipboard copyText={JSON.stringify(rad.record)} popoverText={'Yeah baby!'} />
       </Table.DataCell>
+      <Table.DataCell>
+        <Button variant={'secondary'} onClick={() => addLeft()}>
+          Venstre
+        </Button>
+        <Button variant={'secondary'} onClick={() => addRight()}>
+          Høyre
+        </Button>
+      </Table.DataCell>
     </Table.ExpandableRow>
   );
 };
@@ -44,7 +64,56 @@ interface SoekeresultatProps {
   data: ResultatType[] | undefined;
 }
 
+type ActionType = {
+  type: DiffActions;
+  payload?: ResultatType;
+};
+
+export interface DiffState {
+  leftSide?: ResultatType;
+  rightSide?: ResultatType;
+}
+
+function reducer(state: DiffState, action: ActionType) {
+  switch (action.type) {
+    case DiffActions.ADD_LEFT: {
+      return {
+        ...state,
+        leftSide: action.payload,
+      };
+    }
+    case DiffActions.ADD_RIGHT: {
+      return {
+        ...state,
+        rightSide: action.payload,
+      };
+    }
+    case DiffActions.REMOVE_LEFT: {
+      return {
+        ...state,
+        leftSide: undefined,
+      };
+    }
+    case DiffActions.REMOVE_RIGHT: {
+      return {
+        ...state,
+        rightSide: undefined,
+      };
+    }
+    case DiffActions.CLEAR_ALL: {
+      return {
+        ...state,
+        leftSide: undefined,
+        rightSide: undefined,
+      };
+    }
+    default:
+      console.warn('Unknown action:' + action.type);
+      return state;
+  }
+}
 const Soekeresultat = (props: SoekeresultatProps) => {
+  const [state, dispatch] = useReducer(reducer, {});
   const { sort, handleSort } = useHandleSort();
 
   if (!props.data) {
@@ -54,42 +123,55 @@ const Soekeresultat = (props: SoekeresultatProps) => {
   const sortedData = sortData(props.data, sort);
 
   return (
-    <Table className={styles.resultatTabell} sort={sort} onSortChange={(sortKey) => handleSort(sortKey)}>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell />
-          <Table.HeaderCell />
-          <Table.HeaderCell>Personident</Table.HeaderCell>
-          <Table.ColumnHeader sortKey={'dtoVersion'} sortable>
-            DTO version
-          </Table.ColumnHeader>
-          <Table.ColumnHeader sortKey={'partition'} sortable>
-            Partition
-          </Table.ColumnHeader>
-          <Table.ColumnHeader sortKey={'offset'} sortable>
-            Offset
-          </Table.ColumnHeader>
-          <Table.ColumnHeader sortKey={'topic'} sortable>
-            Topic
-          </Table.ColumnHeader>
-          <Table.ColumnHeader sortKey={'timestamp'} sortable>
-            Timestamp
-          </Table.ColumnHeader>
-          <Table.ColumnHeader sortKey={'systemTimeMs'} sortable>
-            SystemTimeMs
-          </Table.ColumnHeader>
-          <Table.ColumnHeader sortKey={'streamTimeMs'} sortable>
-            StreamTimeMs
-          </Table.ColumnHeader>
-          <Table.ColumnHeader>Kopier JSON</Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {sortedData.map((rad, index) => (
-          <Datarad rad={rad} key={index} />
-        ))}
-      </Table.Body>
-    </Table>
+    <section className={styles.resultatSeksjon}>
+      <Table className={styles.resultatTabell} sort={sort} onSortChange={(sortKey) => handleSort(sortKey)}>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell />
+            <Table.HeaderCell>Personident</Table.HeaderCell>
+            <Table.ColumnHeader sortKey={'dtoVersion'} sortable>
+              DTO version
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={'partition'} sortable>
+              Partition
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={'offset'} sortable>
+              Offset
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={'topic'} sortable>
+              Topic
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={'timestamp'} sortable>
+              Timestamp
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={'systemTimeMs'} sortable>
+              SystemTimeMs
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={'streamTimeMs'} sortable>
+              StreamTimeMs
+            </Table.ColumnHeader>
+            <Table.ColumnHeader>Kopier JSON</Table.ColumnHeader>
+            <Table.ColumnHeader>Diff</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {sortedData.map((rad, index) => (
+            <Datarad
+              rad={rad}
+              key={index}
+              addLeft={() => dispatch({ type: DiffActions.ADD_LEFT, payload: rad })}
+              addRight={() => dispatch({ type: DiffActions.ADD_RIGHT, payload: rad })}
+            />
+          ))}
+        </Table.Body>
+      </Table>
+      <DiffViewer
+        leftSide={state.leftSide}
+        rightSide={state.rightSide}
+        clearAll={() => dispatch({ type: DiffActions.CLEAR_ALL })}
+      />
+    </section>
   );
 };
 
